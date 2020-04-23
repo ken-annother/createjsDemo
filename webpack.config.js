cb17c905773596882c57acba8bcbde3b6f7c93aa
songkun <jackskin@126.com>
2020-04-23 17:55:59 +0800

    修改配置
    Signed-off-by: songkun <jackskin@126.com>

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {
     mode: 'development',
     entry: './src/btree.js',
-    devtool: 'inline-source-map',
+    devtool: 'source-map',
     devServer: {
         contentBase: './dist',
         port: 9900
